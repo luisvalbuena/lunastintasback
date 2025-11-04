622452cfb7fe7bc62b24849adc0d0bb07f9a7489
luisvalbuena <72606794+luisvalbuena@users.noreply.github.com>
2025-11-04 06:46:33 +0100

    Delete src directory

--- src/middleware/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-export function ensureLoggedIn(req, res, next) {
-    if (req.session && req.session.admin) {
-      next();
-    } else {
-      res.status(401).json({ error: 'No autorizado' });
-    }
-  }
-  
